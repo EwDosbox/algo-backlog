bb1d01a9c3c3a5ffc0188c59ebf8257bc7ee50d2
EwDosbox <cada.vviktor@gmail.com>
2026-04-15 16:32:43 +0200

    added a test framework with  MST

---
+++ tests/Program.cs
@@ -0,0 +1,21 @@
+﻿using Heaps;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class MinHeapTests
+{
+    [TestMethod]
+    public void Insert_ShouldMaintainMinProperty()
+    {
+        // Arrange
+        var heap = new MinHeap<string>();
+
+        // Act
+        heap.Insert("Low Priority", 10);
+        heap.Insert("High Priority", 1);
+        heap.Insert("Medium Priority", 5);
+
+        // Assert
+        Assert.AreEqual("High Priority", heap.Extreme());
+    }
+}
